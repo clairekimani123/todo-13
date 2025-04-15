4e85493517bae197787f96435e4d600ecd58b538
claire <claire.kimani@student.moringaschool.com>
2025-04-15 14:59:12 +0300

    adds in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,45 @@ function App() {
 
   const [todos, setTodos] = useState(TODOS);
 
-  console.log(todos);
+  // console.log(todos);
   
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(e);
     
+  // }
+
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newTask.trim()) return
+
+    setTasks([...tasks, {id:Date.now(), text:
+      newTask, completed: false}])
+    setNewTask('')
   }
+ 
 
 
   return (
    <div>
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+         type="text"
+         value={newTask}
+         onChange={(e) =>
+          setNewTask(e.target.value)}
+         placeholder='Add new task...'
+          />
+          <button type='submit'>Add new task</button>
+      </form>
+    </div>
       <form onSubmit={handleSubmit}>
         <input type="text" name="todo" />
         <button type='submit'>Add Todo</button>
